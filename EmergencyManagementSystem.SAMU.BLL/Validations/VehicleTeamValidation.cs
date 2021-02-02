ffc64c4893c2c21a463a1b5e92bce0ff99cf58bb
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-02-02 18:31:37 -0300

    feat: todas as classes de validações criadas.

---
+++ EmergencyManagementSystem.SAMU.BLL/Validations/VehicleTeamValidation.cs
@@ -0,0 +1,11 @@
+﻿using EmergencyManagementSystem.SAMU.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmergencyManagementSystem.SAMU.BLL.Validations
+{
+    public class VehicleTeamValidation : BaseValidation<VehicleTeam>
+    {
+    }
+}
