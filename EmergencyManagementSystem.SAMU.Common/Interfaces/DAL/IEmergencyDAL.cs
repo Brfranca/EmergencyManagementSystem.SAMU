e6b94d1a951d1aae32a37fe52374226a783c465f
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-02-02 18:50:03 -0300

    feat: criação das interfaces DAL

---
+++ EmergencyManagementSystem.SAMU.Common/Interfaces/DAL/IEmergencyDAL.cs
@@ -0,0 +1,12 @@
+﻿using EmergencyManagementSystem.SAMU.Common.Models;
+using EmergencyManagementSystem.SAMU.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmergencyManagementSystem.SAMU.Common.Interfaces.DAL
+{
+    public interface IEmergencyDAL : IBaseDAL<Emergency>
+    {
+    }
+}
---
+++ EmergencyManagementSystem.SAMU.Common/Interfaces/DAL/IVehicleTeamDAL.cs
@@ -0,0 +1,11 @@
+﻿using EmergencyManagementSystem.SAMU.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmergencyManagementSystem.SAMU.Common.Interfaces.DAL
+{
+    public interface IVehicleTeamDAL : IBaseDAL<VehicleTeam>
+    {
+    }
+}
