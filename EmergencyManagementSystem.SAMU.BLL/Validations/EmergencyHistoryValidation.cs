94c6d8abee9fa0b222419601baa0efe990ebb2e5
Kátia Alexsandra Jung <katiajung@outlook.com>
2021-02-03 14:57:12 -0300

    feat:  EmergencyHistoryValidation

--- EmergencyManagementSystem.SAMU.BLL/Validations/EmergencyHistoryValidation.cs
+++ EmergencyManagementSystem.SAMU.BLL/Validations/EmergencyHistoryValidation.cs
@@ -1,4 +1,5 @@
 ﻿using EmergencyManagementSystem.SAMU.Entities.Entities;
+using FluentValidation;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,5 +8,16 @@ namespace EmergencyManagementSystem.SAMU.BLL.Validations
 {
     public class EmergencyHistoryValidation : BaseValidation<EmergencyHistory>
     {
+        public EmergencyHistoryValidation()
+        {
+            RuleFor(d => d.Description)
+                .NotNull()
+                .NotEmpty()
+                .WithMessage("Favor informar a descrição")
+                .Length(3, 60)
+                .WithMessage("A descrição deve conter entre 3 e 60 caracteres");
+
+
+        }
     }
 }
