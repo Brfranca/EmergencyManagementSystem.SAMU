3005cf337bb4f580f9f69f819ba8c7b4fc0ce30b
SlowBits <77354211+SlowBits@users.noreply.github.com>
2021-02-18 04:16:22 -0300

    fix: AddressValidation

--- EmergencyManagementSystem.SAMU.BLL/Validations/AddressValidation.cs
+++ EmergencyManagementSystem.SAMU.BLL/Validations/AddressValidation.cs
@@ -12,8 +12,21 @@ namespace EmergencyManagementSystem.SAMU.BLL.Validations
         public AddressValidation()
         {
             RuleFor(e => e.CEP)
-                .Length(8)
-                .WithMessage("O CEP deve conter 8 números.");
+                .Cascade(CascadeMode.Stop)
+                .Custom((cep, context) =>
+                {
+                    if (!String.IsNullOrWhiteSpace(cep) && cep.Length != 8)
+                    {
+                        context.AddFailure("O CEP deve conter 8 números.");
+                    }
+                })
+                .Custom((cep, context) =>
+                {
+                    if (!String.IsNullOrWhiteSpace(cep) && !IsValidNumber(cep))
+                    {
+                        context.AddFailure("CEP inválido.");
+                    }
+                });
 
             RuleFor(e => e.City)
                 .Cascade(CascadeMode.Stop)
@@ -23,39 +36,56 @@ namespace EmergencyManagementSystem.SAMU.BLL.Validations
                 .Length(3, 100)
                 .WithMessage("A cidade deve ter entre 3 e 100 letras.")
                 .Must(IsValidName)
-                .WithMessage("O nome da cidade não deve conter números e caracteres especiais.");
+                .WithMessage("O nome da cidade não deve conter números ou caracteres especiais.");
 
             RuleFor(e => e.District)
                 .Cascade(CascadeMode.Stop)
                 .MaximumLength(60)
-                .WithMessage("O bairro deve ter no máximo 60 letras.")
-                .Must(IsValidName)
-                .WithMessage("O nome do bairro não deve conter números e caracteres especiais.");
+                .WithMessage("O bairro deve conter no máximo 60 letras.")
+                .Custom((bairro, context) =>
+                {
+                    if (!String.IsNullOrWhiteSpace(bairro) && !IsValidName(bairro))
+                    {
+                        context.AddFailure("O nome do bairro não deve conter números ou caracteres especiais.");
+                    }
+                });
 
             RuleFor(e => e.Complement)
                 .MaximumLength(60)
-                .WithMessage("O complemento deve ter no máximo 60 caractertes.");
+                .WithMessage("O complemento deve conter no máximo 60 caracteres.");
 
             RuleFor(e => e.Number)
                 .MaximumLength(10)
-                .WithMessage("O número deve ter no máximo 10 caractertes.");
+                .WithMessage("O número deve conter no máximo 10 caracteres.")
+                .Custom((number, context) =>
+                {
+                    if (!String.IsNullOrWhiteSpace(number) && !IsValidNumber(number))
+                    {
+                        context.AddFailure("Número inválido.");
+                    }
+                });
 
             RuleFor(e => e.Reference)
                 .MaximumLength(60)
-                .WithMessage("A referência deve ter no máximo 60 caractertes");
+                .WithMessage("A referência deve conter no máximo 60 caracteres");
 
             RuleFor(e => e.Street)
                 .Cascade(CascadeMode.Stop)
                 .NotNull()
                 .NotEmpty()
-                .WithMessage("Favor informar o nome da rua.")
+                .WithMessage("Favor informar a rua.")
                 .Length(3, 60)
-                .WithMessage("A rua deve conter entre 3 e 60 letras.");
+                .WithMessage("A rua deve ter entre 3 e 60 caracteres.");
         }
 
         private bool IsValidName(string Name)
         {
             return Regex.IsMatch(Name, @"^[\p{L} \.\-]+$");
+        }
+
+        private bool IsValidNumber(string Number)
+        {
+            return Regex.IsMatch(Number, @"^[0-9]*$");
         }
     }
 }
