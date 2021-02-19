3ad2b77b20fe76c8c1b070d97ffd27fa251bca94
Beatriz França <beatriz.rbfranca@gmail.com>
2021-02-19 08:24:05 -0300

    Feat:  FindAll medicalEvaluation

--- EmergencyManagementSystem.SAMU.Common/Interfaces/DAL/IMedicalEvaluationDAL.cs
+++ EmergencyManagementSystem.SAMU.Common/Interfaces/DAL/IMedicalEvaluationDAL.cs
@@ -1,13 +1,13 @@
 ﻿using EmergencyManagementSystem.SAMU.Common.Filters;
 using EmergencyManagementSystem.SAMU.Entities.Entities;
-using System;
 using System.Collections.Generic;
-using System.Text;
 
 namespace EmergencyManagementSystem.SAMU.Common.Interfaces.DAL
 {
     public interface IMedicalEvaluationDAL : IBaseDAL<MedicalEvaluation>
     {
         MedicalEvaluation Find(MedicalEvaluationFilter filter);
+
+        List<MedicalEvaluation> FindAll(MedicalEvaluationFilter filter);
     }
 }
