e82ed96e90c9e9c72821595549c40c14eb5b6d4f
Beatriz França <beatriz.rbfranca@gmail.com>
2021-02-23 20:27:18 -0300

    fix: connectionString

--- EmergencyManagementSystem.SAMU.API/Startup.cs
+++ EmergencyManagementSystem.SAMU.API/Startup.cs
@@ -141,12 +141,9 @@ namespace EmergencyManagementSystem.SAMU.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, Context context)
         {
-            if (env.IsDevelopment())
-            {
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "EmergencyManagementSystem.SAMU.API v1"));
-            }
 
             app.UseHttpsRedirection();
 
